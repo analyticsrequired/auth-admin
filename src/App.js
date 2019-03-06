eec1a5ce0058d1de4aa3f47836e94a9e7eb7f508
Kylee Tilley <kyleetilley@gmail.com>
2019-03-05 22:42:23 -0600

    Add login/token views

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useContext, useState, Fragment } from "react";
 import "./App.css";
+import { AuthContext } from "./AuthContext";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function App() {
+  const auth = useContext(AuthContext);
+
+  return auth.token ? <Token /> : <LoginForm />;
+}
+
+function LoginForm() {
+  const auth = useContext(AuthContext);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onSubmit(e) {
+    e.preventDefault();
+    auth.login(username, password);
   }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button>Login</button>
+    </form>
+  );
+}
+
+function Token() {
+  const auth = useContext(AuthContext);
+
+  function onClick(e) {
+    e.preventDefault();
+    auth.logout();
+  }
+
+  return (
+    <Fragment>
+      <p>{auth.token}</p>
+      <button onClick={onClick}>Logout</button>
+    </Fragment>
+  );
 }
 
 export default App;

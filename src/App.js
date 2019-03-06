8f13b2d54c923a704af60b4057174c44b54ff1ba
Kylee Tilley <kyleetilley@gmail.com>
2019-03-06 17:59:21 -0600

    Adding logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 import "./App.css";
 import { AuthContext } from "./AuthContext";
 import LoggedIn from "./LoggedIn";
 import NotLoggedIn from "./NotLoggedIn";
+import logo from "./logo.png";
 
 function App() {
   const auth = useContext(AuthContext);
-  return auth.token ? <LoggedIn /> : <NotLoggedIn />;
+
+  return (
+    <Fragment>
+      <img src={logo} alt="Analytics Required" />
+      {auth.token ? <LoggedIn /> : <NotLoggedIn />}
+    </Fragment>
+  );
 }
 
 export default App;

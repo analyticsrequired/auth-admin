2cb39d47e8d3b4709992958bf034f3021cc76f5f
Kylee Tilley <kyleetilley@gmail.com>
2019-03-06 19:40:36 -0600

    Refactor

--- src/AdminTools.js
+++ src/AdminTools.js
@@ -5,13 +5,9 @@ import { AuthContext } from "./AuthContext";
 export default function AdminTools() {
   const auth = useContext(AuthContext);
 
-  return (
-    <Fragment>
-      {auth.user.permissions.includes("admin") ? (
-        <Segment>
-          <Header as="h2">Admin Tools</Header>
-        </Segment>
-      ) : null}
-    </Fragment>
-  );
+  return auth.user.permissions.includes("admin") ? (
+    <Segment>
+      <Header as="h2">Admin Tools</Header>
+    </Segment>
+  ) : null;
 }

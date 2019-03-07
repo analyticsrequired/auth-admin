b04122629bdff60d9218ba13813ce63b50cdad37
Kylee Tilley <kyleetilley@gmail.com>
2019-03-06 19:36:18 -0600

    UserTools

--- src/LoggedIn.js
+++ src/LoggedIn.js
@@ -1,63 +1,12 @@
-import React, { useContext, Fragment } from "react";
-import styled from "styled-components";
-import { AuthContext } from "./AuthContext";
-import { Segment, Label, Header } from "semantic-ui-react";
-import LogoutButton from "./LogoutButton";
+import React, { Fragment } from "react";
+import UserTools from "./UserTools";
 import AdminTools from "./AdminTools";
 
 export default function Token() {
-  const auth = useContext(AuthContext);
-
   return (
     <Fragment>
-      <Segment>
-        <Header as="h2">User</Header>
-        <Table>
-          <tbody>
-            <tr>
-              <th>Id</th>
-              <th>Permissions</th>
-            </tr>
-            <tr>
-              <td>{auth.user.id}</td>
-              <td>
-                {auth.user.permissions.map(permission => (
-                  <Label>{permission}</Label>
-                ))}
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-      </Segment>
-
-      <Segment>
-        <Header as="h2">Session</Header>
-        <Table>
-          <tbody>
-            <tr>
-              <th style={{ verticalAlign: "top" }}>Token</th>
-              <td>
-                <TokenString>{auth.token}</TokenString>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-        <LogoutButton />
-      </Segment>
-
+      <UserTools />
       <AdminTools />
     </Fragment>
   );
 }
-
-const Table = styled.table`
-  width: 100%;
-  text-align: left;
-`;
-
-const TokenString = styled.span`
-  padding: 0;
-  margin: 0;
-  font-family: monospace;
-  word-break: break-all;
-`;
---
+++ src/UserTools.js
@@ -0,0 +1,61 @@
+import React, { useContext, Fragment } from "react";
+import styled from "styled-components";
+import { AuthContext } from "./AuthContext";
+import { Segment, Label, Header, Divider } from "semantic-ui-react";
+import LogoutButton from "./LogoutButton";
+
+export default function UserTools() {
+  const auth = useContext(AuthContext);
+
+  return (
+    <Fragment>
+      <Segment>
+        <Header as="h2">User</Header>
+        <Table>
+          <tbody>
+            <tr>
+              <th>Id</th>
+              <th>Permissions</th>
+            </tr>
+            <tr>
+              <td>{auth.user.id}</td>
+              <td>
+                {auth.user.permissions.map(permission => (
+                  <Label>{permission}</Label>
+                ))}
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Segment>
+
+      <Segment>
+        <Header as="h2">Session</Header>
+        <Table>
+          <tbody>
+            <tr>
+              <th style={{ verticalAlign: "top" }}>Token</th>
+              <td>
+                <TokenString>{auth.token}</TokenString>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+        <Divider hidden />
+        <LogoutButton />
+      </Segment>
+    </Fragment>
+  );
+}
+
+const Table = styled.table`
+  width: 100%;
+  text-align: left;
+`;
+
+const TokenString = styled.span`
+  padding: 0;
+  margin: 0;
+  font-family: monospace;
+  word-break: break-all;
+`;

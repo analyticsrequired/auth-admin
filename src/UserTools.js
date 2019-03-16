a9291635a1886ccea182c253a19e85c1c3a9d570
Kylee Tilley <kyleetilley@gmail.com>
2019-03-16 10:09:32 -0500

    Redesigning user tools

--- src/UserTools.js
+++ src/UserTools.js
@@ -17,52 +17,56 @@ export default function UserTools() {
   return (
     <Fragment>
       <Segment>
-        <Header as="h2">User</Header>
-        <Table>
-          <tbody>
-            <tr>
-              <th>Id</th>
-              <th>Permissions</th>
-            </tr>
-            <tr>
-              <td>{auth.user.sub}</td>
-              <td>
-                {auth.user.permissions.length
-                  ? auth.user.permissions.map(permission => (
-                      <Label key={permission}>{permission}</Label>
-                    ))
-                  : "-"}
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-      </Segment>
+        <Header as="h2">User Tools</Header>
 
-      <Segment>
-        <Header as="h2">Session</Header>
-        <Table>
-          <colgroup>
-            <col style={{ width: "50%" }} />
-            <col />
-          </colgroup>
-          <tbody>
-            <tr>
-              <th>Access Token</th>
-              <th style={{ textAlign: "right" }}>Expiration</th>
-            </tr>
-            <tr>
-              <td style={{ verticalAlign: "top" }}>
-                <TokenString>{auth.accessToken}</TokenString>
-              </td>
-              <td style={{ verticalAlign: "top", textAlign: "right" }}>
-                <TimeAgo date={auth.user.exp * 1000} />
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-        <Divider hidden />
-        <LogoutButton />
-        <RefreshButton />
+        <Segment>
+          <Header as="h3">Identity</Header>
+          <Table>
+            <tbody>
+              <tr>
+                <th>UserId</th>
+                <th>Permissions</th>
+              </tr>
+              <tr>
+                <td>{auth.user.sub}</td>
+                <td>
+                  {auth.user.permissions.length
+                    ? auth.user.permissions.map(permission => (
+                        <Label key={permission}>{permission}</Label>
+                      ))
+                    : "-"}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Segment>
+
+        <Segment>
+          <Header as="h3">Session</Header>
+          <Table>
+            <colgroup>
+              <col style={{ width: "50%" }} />
+              <col />
+            </colgroup>
+            <tbody>
+              <tr>
+                <th>Access Token</th>
+                <th style={{ textAlign: "right" }}>Expiration</th>
+              </tr>
+              <tr>
+                <td style={{ verticalAlign: "top" }}>
+                  <TokenString>{auth.accessToken}</TokenString>
+                </td>
+                <td style={{ verticalAlign: "top", textAlign: "right" }}>
+                  <TimeAgo date={auth.user.exp * 1000} />
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          <Divider hidden />
+          <LogoutButton />
+          <RefreshButton />
+        </Segment>
       </Segment>
     </Fragment>
   );

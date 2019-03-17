9d7c6800204022a881f126fe900d3f5ef7e8d16f
Kylee Tilley <kyleetilley@gmail.com>
2019-03-16 20:27:53 -0500

    Adding permissions input

---
+++ src/PermissionsInput.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Input, Label, Icon } from "semantic-ui-react";
+
+export default function PermissionInput(props) {
+  const [newPermission, setNewPermission] = useState("");
+
+  const onChange = updateFn => e => updateFn(e.target.value);
+
+  const onClickAdd = e => {
+    e.preventDefault();
+    props.onChange &&
+      props.onChange({ target: { value: [...props.value, newPermission] } });
+    setNewPermission("");
+  };
+
+  const onClickDelete = i => e => {
+    e.preventDefault();
+    props.onChange &&
+      props.onChange({
+        target: {
+          value: props.value.filter(
+            (_, permissionIndex) => permissionIndex !== i
+          )
+        }
+      });
+  };
+
+  return (
+    <div>
+      {props.value.map((permission, i) => (
+        <Label>
+          {permission}
+          <Icon key={i} onClick={onClickDelete(i)} name="delete" />
+        </Label>
+      ))}
+
+      <Input
+        type="text"
+        label="Permission"
+        value={newPermission}
+        onChange={onChange(setNewPermission)}
+        action={{
+          children: "Add",
+          onClick: onClickAdd
+        }}
+      />
+    </div>
+  );
+}
+
+PermissionInput.propTypes = {
+  value: PropTypes.arrayOf(PropTypes.string)
+};
